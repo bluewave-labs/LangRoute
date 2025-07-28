af080e6c45835fe1091bb6b3d36a814ec584e208
Sepideh Shahbazi <sepideh.shahbazi@gmail.com>
2025-07-27 20:17:27 -0700

    feat(ui): add shadcn base components and MultipleSelector component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,7 @@ export { default as FormInput } from './form/FormInput';
 export { Button } from './ui/button';
 export { Input } from './ui/input';
 export { Label } from './ui/label';
+export { Dialog } from './ui/dialog';
+export { Badge } from './ui/badge';
+export { Command } from './ui/command';
+export { default as MultipleSelector } from './ui/multiple-selector';
