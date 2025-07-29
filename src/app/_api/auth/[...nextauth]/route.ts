3112c62c93173f50eb6e03b2cb4e3f45b182968b
Mahid Ahmad <mahid797@hotmail.com>
2025-07-29 05:25:48 -0400

    feat(Auth): Implement password reset, registration, and user session retrieval endpoints

---
+++ src/app/_api/auth/[...nextauth]/route.ts
@@ -0,0 +1,8 @@
+import NextAuth from 'next-auth';
+
+import { authOptions } from '@/lib/auth';
+
+const handler = NextAuth(authOptions) as unknown as (req: Request) => Promise<Response>;
+
+export const GET = handler;
+export const POST = handler;
