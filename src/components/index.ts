640cfca2aca88c7c8324f71e6f5760ec1be22613
Sepideh Shahbazi <sepideh.shahbazi@gmail.com>
2025-08-11 02:39:50 -0700

    Key Management: add page and modal (#21)
    * feat(key-management): add page and modal
    
    * feat(icons): add SearchIcon and EmptyStateIcon
    
    * feat(components): add reusable components
    
    * feat(ui): add shadcn base components
    
    * chore: minor changes
    
    ---------
    
    Co-authored-by: Mahid Ahmad <mahid.ahmad797@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,13 @@
+export { default as SearchBar } from './input/SearchBar';
+
+export { default as EmptyState } from './common/EmptyState';
+export { default as PageHeader } from './common/PageHeader';
+
+export { Separator } from './ui/separator';
+export { Table } from './ui/table';
+export { Select } from './ui/select';
+export { Sheet } from './ui/sheet';
+
 export { default as FormInput } from './form/FormInput';
 
 // Auth components
@@ -6,3 +16,4 @@ export { GoogleSignInButton } from './auth/GoogleSignInButton';
 export { Button } from './ui/button';
 export { Input } from './ui/input';
 export { Label } from './ui/label';
+
