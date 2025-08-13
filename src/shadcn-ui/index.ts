547f5ab50691d60047ca2caa2d303e560ef79895
Mahid Ahmad <mahid797@hotmail.com>
2025-08-13 15:34:59 -0400

    task: Restructured ShadCn components

---
+++ src/shadcn-ui/index.ts
@@ -0,0 +1,98 @@
+// Badge
+export { Badge, badgeVariants } from './badge';
+
+// Button
+export { Button, buttonVariants } from './button';
+
+// Card
+export {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from './card';
+
+// Command
+export {
+	Command,
+	CommandDialog,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+	CommandSeparator,
+	CommandShortcut,
+} from './command';
+
+// Dialog
+export {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogOverlay,
+	DialogPortal,
+	DialogTitle,
+	DialogTrigger,
+} from './dialog';
+
+// Input
+export { Input } from './input';
+
+// Label
+export { Label } from './label';
+
+// Multiple Selector
+export { default as MultipleSelector } from './multiple-selector';
+export type { Option, MultipleSelectorRef } from './multiple-selector';
+export { useDebounce } from './multiple-selector';
+
+// Select
+export {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectScrollDownButton,
+	SelectScrollUpButton,
+	SelectSeparator,
+	SelectTrigger,
+	SelectValue,
+} from './select';
+
+// Separator
+export { Separator } from './separator';
+
+// Sheet
+export {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from './sheet';
+
+// Table
+export {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableFooter,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from './table';
+
+// Tabs
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs';
